fdf38b58a519c3c745e1a7e0b83229bcf9967283
Raphael Kawabata <raphael.kawabata@gmail.com>
2021-08-25 20:26:46 +0000

    completing the functions in the process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,77 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    """
+    description: receives 2 data sets, message and categories,
+    and merge them to one data set.
+    
+    Inputs:
+    messages_filepath: a csv file path containing the message data set
+    categories_filepath: a csv file path containing the category data set
+    
+    Output:
+    df: a merged data set using the input data set
+   
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge the data sets to one data set
+    df = messages.merge(categories, on='id')
+    
+    return df
 
 def clean_data(df):
-    pass
+    """
+    description: Data cleaning function
 
+    Inputs: a merged data frame (message + category)
 
+    Output: cleaned data frame
+
+    """    
+    # get right categories column name
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.loc[0, :]
+    category_colnames = [(lambda row : category.split('-')[0])(category) for label, category in row.iteritems()]
+    
+    # set new column names
+    categories.columns = category_colnames
+    
+    # get categories columns values
+    for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = [(lambda categories : value_str.split('-')[1])(value_str) 
+                          for label, value_str in categories[column].iteritems()]
+    
+    # convert column from string to numeric
+    categories[column] = categories[column].astype(int)
+    
+    # drop the original categories column from `df`
+    df.drop(columns='categories', inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # remove duplicated data
+    df.drop_duplicates(subset=['message', 'original'], inplace=True)
+    
+    return df
+    
 def save_data(df, database_filename):
-    pass  
+    """
+    description: it saves the data set to sql database file
+    
+    Inputs: a merged data frame (message + category)
+    
+    output: creates a sql database file, containing the data from
+    input data set
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql(database_filename[:-3], engine, index=False, if_exists='replace')
 
 
 def main():
